b9497e83026e0feef51330938f6d6a119b38751e
Ubuntu <abrarhassan7809@gmail.com>
2025-12-15 11:29:57 +0500

    pixel adventure game

---
+++ lib/components/collision_block.dart
@@ -0,0 +1,7 @@
+import 'package:flame/components.dart';
+
+class CollisionBlock extends PositionComponent {
+  CollisionBlock({position, size, this.isPlatform = false}) : super(position: position, size: size,);
+  bool isPlatform;
+
+}
